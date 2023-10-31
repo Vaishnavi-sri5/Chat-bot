f5c757f4a8f3845d06240c61fe2831ac69f14f32
Vaishnavi Srivastava <144820270+Vaishnavi-sri5@users.noreply.github.com>
2023-10-31 23:52:55 +0530

    script.js

--- chatbot/script.js
+++ chatbot/script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "API key"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
